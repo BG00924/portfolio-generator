4fdf59fbeb400ce5db6031abed49354a077c86c3
BG00924 <BG00924@users.noreply.github.com>
2020-07-27 19:04:08 -0500

    second lesson

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length)
-//console.log(profileDataArgs)
+const fs = require('fs')
+const generatePage = require('./src/page-template')
 
-const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i +=1) {
-        console.log(profileDataArr[i])
-    }
+//Lesson 1 Code
+const profileDataArgs = process.argv.slice(2)
+// //console.log(profileDataArgs)
+// const printProfileData = (profileDataArr) => {
+//     for (let i = 0; i < profileDataArr.length; i +=1) {
+//         console.log(profileDataArr[i])
+//     }
+//     console.log('===================================')
+//     profileDataArr.forEach(profileItem =>console.log(profileItem))
+// }
+// printProfileData(profileDataArgs)
+// End Lesson 1 code
 
-    console.log('===================================')
-
-    profileDataArr.forEach(profileItem =>console.log(profileItem))
-}
+const [name, github] = profileDataArgs
 
 
 
-printProfileData(profileDataArgs)
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw err;
+    
+    console.log('Portfolio complete! Checkout index.html to see the output!')
+})
