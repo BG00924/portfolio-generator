4db6bf0a5f448512d4f664ecb86ab966955eeb9b
BG00924 <BG00924@users.noreply.github.com>
2020-07-29 11:01:22 -0500

    installed inquirer

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const fs = require('fs')
-const generatePage = require('./src/page-template')
+// const fs = require('fs')
+const inquirer = require('inquirer')
+// console.log(inquirer)
+// const generatePage = require('./src/page-template')
 
-//Lesson 1 Code
-const profileDataArgs = process.argv.slice(2)
+// //Lesson 1 Code
+// const profileDataArgs = process.argv.slice(2)
 // //console.log(profileDataArgs)
 // const printProfileData = (profileDataArr) => {
 //     for (let i = 0; i < profileDataArr.length; i +=1) {
@@ -14,12 +16,23 @@ const profileDataArgs = process.argv.slice(2)
 // printProfileData(profileDataArgs)
 // End Lesson 1 code
 
-const [name, github] = profileDataArgs
+// const [name, github] = profileDataArgs
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
     
-    console.log('Portfolio complete! Checkout index.html to see the output!')
-})
+//     console.log('Portfolio complete! Checkout index.html to see the output!')
+// })
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers))
